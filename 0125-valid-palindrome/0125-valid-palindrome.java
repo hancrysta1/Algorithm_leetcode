7e43e2ef7a882d6efe0c8f03e4ab21a3defd5713
SooJeong Han <105469888+hancrysta1@users.noreply.github.com>
2024-12-11 22:53:21 +0900

    Time: 17 ms (15.15%), Space: 45.2 MB (18.59%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Solution {
     public boolean isPalindrome(String s) {
-        String words = s.toLowerCase().replaceAll("[^A-Za-z]","");
+        String words = s.toLowerCase().replaceAll("[^0-9A-Za-z]","");
         //System.out.println(words);
         Deque<Character> stack = new ArrayDeque<>();
         for(int i=0;i<words.length();i++){
